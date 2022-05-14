636645263299f75442ac92d7cc0c7ce7a4a625f3
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2022-05-14 17:52:44 +0100

    User Input

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type your username and press enter
+            Console.WriteLine("Enter your username: ");
+
+            //Create a string variable and get user input from the keyboard and store it in the variable
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int age =  Convert.ToInt32(Console.ReadLine());
+
+            //Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is : " + username + " and His age is " + age);
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int myNum = 15;
+            //myNum = 80;//myNum is now 80 
+
+            /*const int myNum = 15;
+            myNum = 20; // error*/
+
+            /*String fname = "John ";
+            String lname = "Doe";
+            String fullname = fname + lname;*/
+
+            /*int x = 20;
+            int y = 80;*/
+            // int x = 20, y = 80;
+            // same value: int x,y; x=y=200;
+            //Console.WriteLine(x + y);
+
+
+            //Automatic Cascading
+            int mInt = 10;
+            double mDouble = mInt; //int to double
+            Console.WriteLine(mDouble);
+
+            //Manual Cascading
+            double myDouble = 8.8;
+            int myInt =(int) myDouble; //double to int
+            Console.WriteLine(myInt);
+
+            //Type conversion Method
+            int myInt2 = 10;
+            double myDouble2 = 5.25;
+            bool myBool = true;
+
+            Console.WriteLine("-----");
+
+            Console.WriteLine(Convert.ToString(myInt2)); //convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt2)); //convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble2)); //convert double to int
+            Console.WriteLine(Convert.ToString(myBool)); //convert boolean to string
+
+        }
+    }
+}
