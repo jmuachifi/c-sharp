0f4b16e89b1423daee57bfb4a086fb7ed1b34e6d
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2022-05-14 18:16:31 +0100

    Math Functions

---
+++ Math/Math/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max(5,10) = " + Math.Max(5,10));
+            Console.WriteLine("---");
+            Console.WriteLine("Min(5,10) = " + Math.Min(5, 10));
+            Console.WriteLine("---");
+            Console.WriteLine("Sqrt(144) = " + Math.Sqrt(144));
+            Console.WriteLine("---");
+            Console.WriteLine("Abs(-5) = " + Math.Abs(-5));
+            Console.WriteLine("---");
+            Console.WriteLine("Round(99.9) = " + Math.Round(99.9));
+        }
+    }
+}
